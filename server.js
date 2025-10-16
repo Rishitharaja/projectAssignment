432c7d7a9ad963d88250b449c5705d072cd46fb1
pavan <pavankoramutla099@gmail.com>
2025-10-16 19:54:26 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+// server.js
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import authRoutes from "./routes/authRoutes.js";
+import { connectDB } from "./db/dbConfig.js";
+//import { swaggerUi, swaggerSpec } from "./swagger/swaggerConfig.js";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/api/user", authRoutes);
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`App running on port: ${PORT}`);
+    console.log(`Swagger Docs → http://localhost:${PORT}/api-docs`);
+  });
+});
---
+++ swagger/swaggerConfig.js
@@ -0,0 +1,35 @@
+// swaggerConfig.js
+// import swaggerJsdoc from "swagger-jsdoc";
+// import swaggerUi from "swagger-ui-express";
+
+// const options = {
+//   definition: {
+//     openapi: "3.0.0",
+//     info: {
+//       title: "Terralogic Task Management API",
+//       version: "1.0.0",
+//       description: "API documentation for Terralogic Task Management Backend",
+//     },
+//     servers: [
+//       { url: "http://localhost:8080", description: "Local Server" },
+//       {
+//         url: "https://node-js-api-hg8u.onrender.com",
+//         description: "Production Server",
+//       },
+//     ],
+//     components: {
+//       securitySchemes: {
+//         bearerAuth: {
+//           type: "http",
+//           scheme: "bearer",
+//           bearerFormat: "JWT",
+//         },
+//       },
+//     },
+//   },
+//   apis: ["./routes/*.js"], // path to your route files
+// };
+
+// const swaggerSpec = swaggerJsdoc(options);
+
+// export { swaggerUi, swaggerSpec };
