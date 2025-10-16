1c660627d76e92175743e2d2b183e4347e0281f7
pavan <pavankoramutla099@gmail.com>
2025-10-16 20:17:45 +0530

    first commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
 import { connectDB } from "./db/dbConfig.js";
-//import { swaggerUi, swaggerSpec } from "./swagger/swaggerConfig.js";
+import { swaggerUi, swaggerSpec } from "./swagger/swaggerConfig.js";
 
 dotenv.config();
 const app = express();
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use("/api/user", authRoutes);
 
 connectDB().then(() => {
